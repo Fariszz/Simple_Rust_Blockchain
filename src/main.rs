907aa5e8f60c62c330ce17482ab88a7ea1abdd3a
Muhammad Faris <farishadimulyo>
2024-11-29 14:04:19 +0700

    feat: implement chain validator

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,32 @@ mod block;
 use std::collections::LinkedList;
 use block::Block;
 
+const DIFFICULTY: u128 = 5;
+
 fn main() {
     let mut blockchain: LinkedList<Block> = LinkedList::new();
 
-    blockchain.push_back(Block::new("".to_string(), "hello world".to_string()));
+    blockchain.push_back(Block::new("0".to_string(), "hello world".to_string()));
+    println!("Trying to mine block 1...");
+    blockchain.back_mut().unwrap().mine_block(DIFFICULTY);
 
     blockchain.push_back(Block::new(blockchain.back().unwrap().hash.clone(), "hello world 2".to_string()));
+    println!("Trying to mine block 2...");
+    blockchain.back_mut().unwrap().mine_block(DIFFICULTY);
 
     blockchain.push_back(Block::new(blockchain.back().unwrap().hash.clone(), "hello world 3".to_string()));
+    println!("Trying to mine block 3...");
+    blockchain.back_mut().unwrap().mine_block(DIFFICULTY);
+
+    if Block::is_chain_valid(&blockchain, DIFFICULTY) {
+        println!("\nBlockchain is valid");
+    } else {
+        println!("\nBlockchain is not valid");
+    }
 
     for block in &blockchain {
         let serialize = serde_json::to_string_pretty(&block).unwrap();
 
-        println!("{}", serialize);
+        println!("\n{}", serialize);
     }
 }
