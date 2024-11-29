497a8bf8e62c6badb49a616270725c29603e1b4f
Muhammad Faris <farishadimulyo>
2024-11-29 14:03:08 +0700

    feat: add function to valid chain

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,7 @@
 use sha256::digest;
 use std::time::{SystemTime, UNIX_EPOCH};
 use serde::{Serialize, Deserialize};
+use std::collections::LinkedList;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Block {
@@ -24,10 +25,50 @@ impl Block {
             timestamp
         }
     }
+
+    pub fn mine_block(&mut self, difficulty: u128) {
+        let target = "0".repeat(difficulty as usize); // Membuat string target dengan `difficulty` jumlah "0"
+        while &self.hash[0..difficulty as usize] != target {
+            self.timestamp += 1;
+            self.hash = calculate_hash(self.prev_hash.clone(), self.data.clone(), self.timestamp);
+        }
+        println!("Block Mined!!! : {}", self.hash);
+    }
+
+    pub fn is_chain_valid(blockchain: &LinkedList<Block>, difficulty: u128) -> bool {
+        let target = "0".repeat(difficulty as usize); // Membuat string target dengan `difficulty` jumlah "0"
+
+          // Loop melalui blockchain untuk memeriksa hash:
+          for i in 1..blockchain.len() {
+            let current_block = blockchain.iter().nth(i).unwrap();
+            let previous_block = blockchain.iter().nth(i - 1).unwrap();
+
+            // Bandingkan hash terdaftar dan hash yang dihitung:
+            if current_block.hash != calculate_hash(current_block.prev_hash.clone(), current_block.data.clone(), current_block.timestamp) {
+                println!("Current Hashes not equal");
+                return false;
+            }
+
+            // Bandingkan previous hash dan previous hash yang terdaftar
+            if previous_block.hash != current_block.prev_hash {
+                println!("Previous Hashes not equal");
+                return false;
+            }
+
+            // Periksa apakah hash telah diselesaikan
+            if &current_block.hash[0..difficulty as usize] != target {
+                println!("This block hasn't been mined");
+                return false;
+            }
+        }
+        true
+    }
+
+    
 }
 
 // create hash data from block
-pub fn calculate_hash(previous_hash: String, data: String, timestamp: u128) -> String {
+fn calculate_hash(previous_hash: String, data: String, timestamp: u128) -> String {
     let input = format!("{}{}{}", previous_hash, data, timestamp);
     digest(&input)
 }
